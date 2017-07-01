a02bc7885f3d0e8104456840ac9adf54f3b46897
mumu <felipemumuradl@hotmail.com>
2017-07-01 14:20:52 -0300

    Exercise 5: ready

---
+++ exerc5/exerc5.py
@@ -0,0 +1,12 @@
+'''
+Python function isVowel2(char) that returns True if char is a vowel ('a', 'e', 'i', 'o', or 'u'), and False otherwise. 
+You can assume that char is a single letter of any case (ie, 'A' and 'a' are both valid). 
+'''
+def isVowel2(char):
+    '''
+    char: a single letter of any case
+
+    returns: True if char is a vowel and False otherwise.
+    '''
+    # Your code here
+    return char in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
